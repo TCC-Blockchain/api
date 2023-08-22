76ab0399aa98345f6f24f65d19d78c63451fd213
le <le@pop-os.localdomain>
2023-08-22 20:31:03 -0300

    feat: added initial migration

---
+++ prisma/migrations/20230822232929_initial_relations/migration.sql
@@ -0,0 +1,80 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('ADMIN', 'MANAGER', 'EMPLOYEE', 'COMMON');
+
+-- CreateTable
+CREATE TABLE "Address" (
+    "id" TEXT NOT NULL,
+    "street" TEXT NOT NULL,
+    "number" TEXT NOT NULL,
+    "neighbourhood" TEXT NOT NULL,
+    "state" TEXT NOT NULL,
+    "country" TEXT NOT NULL,
+
+    CONSTRAINT "Address_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "RegistryOffice" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "logo" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "document" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "address_id" TEXT,
+
+    CONSTRAINT "RegistryOffice_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Document" (
+    "id" TEXT NOT NULL,
+    "hash_id" TEXT NOT NULL,
+    "url" TEXT NOT NULL,
+    "owner_id" TEXT NOT NULL,
+
+    CONSTRAINT "Document_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Employee" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "document" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "role" "Role" NOT NULL,
+    "registry_office_id" TEXT,
+
+    CONSTRAINT "Employee_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "document" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "registry_office_id" TEXT,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "RegistryOffice_address_id_key" ON "RegistryOffice"("address_id");
+
+-- AddForeignKey
+ALTER TABLE "RegistryOffice" ADD CONSTRAINT "RegistryOffice_address_id_fkey" FOREIGN KEY ("address_id") REFERENCES "Address"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Document" ADD CONSTRAINT "Document_owner_id_fkey" FOREIGN KEY ("owner_id") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Employee" ADD CONSTRAINT "Employee_registry_office_id_fkey" FOREIGN KEY ("registry_office_id") REFERENCES "RegistryOffice"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "User" ADD CONSTRAINT "User_registry_office_id_fkey" FOREIGN KEY ("registry_office_id") REFERENCES "RegistryOffice"("id") ON DELETE SET NULL ON UPDATE CASCADE;
