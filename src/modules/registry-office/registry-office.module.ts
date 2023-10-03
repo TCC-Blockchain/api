0d8c1a48918275c6e5f84c05613441ee69cc9375
Emanuela <lopez.emanuela@gmail.com>
2023-10-03 15:22:44 -0300

    Correções CRUD registry_office

--- src/modules/registry-office/registry-office.module.ts
+++ src/modules/registry-office/registry-office.module.ts
@@ -2,10 +2,14 @@ import { DatabaseModule } from '@infra/database/database.module';
 import { CreateRegistryOffice } from '@modules/registry-office/use-cases/create-registry-office';
 import { Module } from '@nestjs/common';
 import { ProvidersModule } from '@providers/providers.module';
+import { DeleteRegistryOffice } from './use-cases/delete-registry-office';
+import { UpdateRegistryOffice } from './use-cases/update-registry-office';
+import { GetRegistryOfficeById } from './use-cases/get-registry-office-by-id';
+import { GetRegistryOfficeByName } from './use-cases/get-registry-office-by-name';
 
 @Module({
     imports: [DatabaseModule, ProvidersModule],
-    providers: [CreateRegistryOffice],
-    exports: [CreateRegistryOffice],
+    providers: [CreateRegistryOffice, DeleteRegistryOffice, UpdateRegistryOffice, GetRegistryOfficeById, GetRegistryOfficeByName],
+    exports: [CreateRegistryOffice, DeleteRegistryOffice, UpdateRegistryOffice, GetRegistryOfficeById, GetRegistryOfficeByName],
 })
 export class RegistryOfficeModule {}
